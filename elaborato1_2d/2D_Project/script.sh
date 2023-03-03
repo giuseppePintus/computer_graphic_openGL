ffc987c0af693ab5ec9bb34479bd949858c2ed5f
giuseppe pintus <giuseppe.pintus2@studio.unibo.it>
2023-03-03 16:45:57 +0100

    added read me for project 2d

---
+++ elaborato1_2d/2D_Project/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# List of required libraries
+LIBS=(libglu1-mesa-dev libglew-dev libgl1-mesa-dev freeglut3-dev )
+
+# Verify presence of required libraries
+for lib in ${LIBS[@]}; do
+    if ! dpkg -s $lib > /dev/null 2>&1; then
+        echo "Installing $lib..."
+        sudo apt-get install -y $lib
+    fi
+done
+
+# Compile the code using the provided makefile
+make
+
+# Run the resulting executable
+./exe
+
